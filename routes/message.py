f0a4280e5694f7ada7295f24eea5958941b7aade
pablaw <chulhwa7@gmail.com>
2022-10-18 14:45:27 +0900

    chore: 로컬 개발환경 세팅

--- routes/message.py
+++ routes/message.py
@@ -6,3 +6,7 @@ message = Blueprint("message", __name__, template_folder="templates")
 @message.route('/')
 def msg():
     return render_template("message.html")
+
+@message.route('/test')
+def test():
+    return 'this is test'

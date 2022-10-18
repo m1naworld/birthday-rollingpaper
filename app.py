cef379557d7125e4d0e3cb3323217ba0a82fd353
SooBin An <jejupalette@naver.com>
2022-10-18 15:54:54 +0900

    feat : login 페이지 기본 형태 제작

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from routes.homework import homework
 from routes.mars import mars
 from routes.message import message
+from routes.login import login
 app = Flask(__name__)
 
 app.register_blueprint(homework, url_prefix='/homework')
 app.register_blueprint(mars, url_prefix="/mars")
 app.register_blueprint(message, url_prefix="/message")
-
+app.register_blueprint(login, url_prefix="/login")
 @app.route("/")
 def hello_world():
     return "hello world"

3b36dc051d41aeb14f5f7518c5f1807d4c1c4e5c
pablaw <chulhwa7@gmail.com>
2022-10-20 17:57:44 +0900

    fix: 라우터 수정 및 불필요한 주석제거

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 
-from routes.message import messageBP
+from routes.message import message
 from routes.cake import cake
 from routes.login import login
 from routes.rollingpaper import rolling
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 app.register_blueprint(login, url_prefix="/login")
 app.register_blueprint(cake, url_prefix="/cake")
-app.register_blueprint(messageBP, url_prefix="/message")
+app.register_blueprint(message, url_prefix="/message")
 app.register_blueprint(rolling, url_prefix="/rolling")
 app.register_blueprint(sign_up, url_prefix="/sign-up")
 

d039651e026b6b8825e522e53cb54d0ac1657498
mina <m1nna@naver.com>
2022-10-20 15:44:53 +0900

    merge

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 
 from routes.message import messageBP
-
+from routes.cake import cake
+from routes.login import login
+from routes.rollingpaper import rolling
+from routes.sign_up import sign_up
 
 app = Flask(__name__)
 
+app.register_blueprint(login, url_prefix="/login")
+app.register_blueprint(cake, url_prefix="/cake")
 app.register_blueprint(messageBP, url_prefix="/message")
+app.register_blueprint(rolling, url_prefix="/rolling")
+app.register_blueprint(sign_up, url_prefix="/sing-up")
 
 @app.route("/")
 def hello_world():
-    return "hello world"
+    return "hello world!"
 
 @app.route("/message")
 def msg():
@@ -21,4 +28,4 @@ def msg():
 
 if __name__ == '__main__':
     # 초기세팅
-    app.run('0.0.0.0', port=9999, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)

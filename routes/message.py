6bd997130912cbe95754af0c77ed8a039754eccb
yejin0718 <a89557400@gmail.com>
2022-10-24 15:44:29 +0900

    메세지 데이터 저장 기능 구현

--- routes/message.py
+++ routes/message.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify
 
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://pablaw:9dlrhd!357@cluster0.spvewgv.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+from db import db
 
 # 초기 세팅
 messageBP = Blueprint("message", __name__, template_folder="templates")
@@ -19,17 +17,21 @@ def test_fn():
 
 @messageBP.route("/save_msg", methods=["POST"])
 def msg_post():
-    name_receive = request.form['name_give']
-    pw_receive = request.form['pw_give']
-    candle_receive = request.form['candle_give']
+    name_receive = request.form['nick_give']
+    pw_receive = request.form['pwd_give']
     msg_receive = request.form['msg_give']
+    candle_receive = request.form['candle_give']
+
 
     doc = {
-        'name': name_receive,
-        'pw': pw_receive,
-        'candle': candle_receive,
-        'msg': msg_receive
+        'nickname': name_receive,
+        'message_password': pw_receive,
+        'content': msg_receive,
+        'candle_id': candle_receive
     }
-    db.rolling.insert_one(doc)
+    db.message.insert_one(doc)
 
     return jsonify({'msg':'저장 완료!'})
+
+
+

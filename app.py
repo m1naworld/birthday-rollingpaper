af5a0cc371425a75049d70713078675502145634
SooBin An <jejupalette@naver.com>
2022-10-24 22:42:58 +0900

    feat : my page 추가

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from routes.cake import cake
 from routes.login import login
 from routes.rollingpaper import rolling
 from routes.sign_up import sign_up
+from routes.mypage import mypage
 
 from flask_jwt_extended import JWTManager
 import os
@@ -16,6 +17,7 @@ app.register_blueprint(cake, url_prefix="/cake")
 app.register_blueprint(message, url_prefix="/message")
 app.register_blueprint(rolling, url_prefix="/rolling")
 app.register_blueprint(sign_up, url_prefix="/sign-up")
+app.register_blueprint(mypage, url_prefix="/mypage")
 
 # jwt 관련
 app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')

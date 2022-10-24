00446e16d45a8488a8a091faa78f7cbd7d335136
SooBin An <jejupalette@naver.com>
2022-10-24 22:45:02 +0900

    feat : 로그인 시 비밀번호 암호화 및 일치 확인 추가

--- routes/login.py
+++ routes/login.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, jsonify, Flask
 from db import db
+import os
 from dotenv import load_dotenv
-from flask_bcrypt import Bcrypt
+import bcrypt
 from flask_jwt_extended import (
     create_access_token, create_refresh_token, jwt_required, get_jwt_identity,
 )
@@ -15,11 +16,26 @@ def question():
 # jwt 관련
 @login.route("/token", methods=["POST"])
 def create_token():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
     user_id = request.json.get("user_id", None)
     password = request.json.get("password", None)
+    print(password)
 
-    if user_id != 'test' or password != 'test':
-        return jsonify({'msg': '아이디나 비밀번호가 일치하지 않습니다'}), 401
+    if not user_id:
+        return jsonify({"msg": "아이디를 입력해주세요"}), 400
+    if not password:
+        return jsonify({"msg": "비밀번호를 입력해주세요"}), 400
+
+    user = db.user.find_one({'user_id': user_id}) # 나중에 수정!
+    userId = user['user_id']
+    passWord = user['password']
+
+    ispasswordcorrect = bcrypt.checkpw(password.encode('utf-8'), passWord)
+
+    if user_id != userId or ispasswordcorrect != True:
+        return jsonify({'msg': '아이디가 없거나 일치하는 비밀번호가 없습니다'}), 401
 
     access_token = create_access_token(identity=user_id)
     refresh_token = create_refresh_token(identity=user_id)

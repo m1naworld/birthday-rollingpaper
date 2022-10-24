7e576d0607bc318b1cec42c75ef0ad5e496093bc
SooBin An <jejupalette@naver.com>
2022-10-24 11:35:41 +0900

    feat : acess token 세션 스토리지 저장

--- routes/login.py
+++ routes/login.py
@@ -3,7 +3,7 @@ from db import db
 from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import (
-    create_access_token, jwt_required, get_jwt_identity,
+    create_access_token, create_refresh_token, jwt_required, get_jwt_identity,
 )
 
 login = Blueprint("login", __name__, template_folder="templates")
@@ -17,9 +17,11 @@ def question():
 def create_token():
     user_id = request.json.get("user_id", None)
     password = request.json.get("password", None)
-    if user_id != "test" or password != "test":
-        return jsonify({"msg": "Bad username or password"}), 401
+
+    if user_id != 'test' or password != 'test':
+        return jsonify({'msg': '아이디나 비밀번호가 일치하지 않습니다'}), 401
 
     access_token = create_access_token(identity=user_id)
-    return jsonify(access_token=access_token)
+    refresh_token = create_refresh_token(identity=user_id)
+    return jsonify(access_token=access_token, refresh_token=refresh_token), 200
 

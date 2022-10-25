5e8a988ebd6efcedc94a87fc84e1de9151551818
pablaw <chulhwa7@gmail.com>
2022-10-25 13:59:25 +0900

    feat: 케이크, 닉네임, 생일 DB 입력 기능

--- routes/cake.py
+++ routes/cake.py
@@ -7,3 +7,23 @@ cake = Blueprint("cake", __name__, template_folder="templates")
 @cake.route('/')
 def question():
     return render_template("cake.html")
+
+@cake.route("/cake", methods=["GET"])
+def find_user():
+    temp_user = list(db.temp_user.find({},{'_id':False}))
+
+    return jsonify(temp_user)
+
+@cake.route("/cake", methods=["POST"])
+def sel_cake():
+    cake_receive = request.form['cake_give']
+    nickname_receive = request.form['nickname_give']
+    birth_receive = request.form['birth_give']
+
+    user_id = request.form['temp_user']
+
+    db.rollingpaper.update_one({'user_id': user_id}, {'$set': {'cake_id': cake_receive}})
+    db.rollingpaper.update_one({'user_id': user_id}, {'$set': {'user_nickname': nickname_receive}})
+    db.rollingpaper.update_one({'user_id': user_id}, {'$set': {'birth': birth_receive}})
+
+    return jsonify({'msg': '케이크가 만들어졌어요!'})

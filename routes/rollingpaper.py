697f6c91a8b8a3d3dfa8b6071b0fff5d4a326bd4
mina <m1nna@naver.com>
2022-10-25 23:36:39 +0900

    fix: 메인 페이지 기본 정보 GET API

--- routes/rollingpaper.py
+++ routes/rollingpaper.py
@@ -1,23 +1,22 @@
 from flask import Blueprint, render_template, request, jsonify
 from db import db
 
-
 rolling = Blueprint("rolling", __name__, template_folder="templates")
 
-# 롤링 페이퍼 페이지 get
+
+# 롤링 페이퍼 페이지 기본 정보 get
 @rolling.route('/<url>')
 def get_rollingpaper(url):
     print(url)
 
     # test 주석
-    # result = {'url': "mina", 'user_nickname': "mina", "cake_id": "choco"}
-    # message_data = list(db.message.find({'rolling_id': 4}, {'_id': False}))
+    # result = {'url': 'mina', 'rolling_id': 4, 'user_nickname': "mina", "cake_id": "choco"}
+    # message_count = db.message.count_documents({'rolling_id': 4})
 
     result = db.rollingpaper.find_one({'url': url})
-    message_data = list(db.message.find({'rolling_id': result['rolling_id']}))
+    message_count = db.message.count_documents({'rolling_id': result['rolling_id']})
 
     print(result)
-    print(message_data)
+    print(message_count)
 
-    return render_template("guestMain.html", main_data=result, message_data=message_data, message_count=len(message_data))
-
+    return render_template("guestMain.html", mainpage_info=result, message_count=message_count), 200

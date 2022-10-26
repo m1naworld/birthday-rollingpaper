2598a617a23ae5b4666e414423c6d034f1752379
mina <m1nna@naver.com>
2022-10-26 09:46:16 +0900

    feat: 메인페이지 - message 수정 API, 삭제 API

--- routes/rollingpaper.py
+++ routes/rollingpaper.py
@@ -20,10 +20,10 @@ def get_rollingpaper(url):
     print(message_count)
 
     # if(# 토큰 없을 경우 ):
-    #     return render_template("guestMain.html", mainpage_info=result, message_count=message_count), 200
-    #
+    return render_template("guestMain.html", mainpage_info=result, message_count=message_count), 200
+
     # else: # 토큰 있을 경우
-    return render_template("rollingpaper.html", mainpage_info=result, message_conut=message_count), 200
+    # return render_template("rollingpaper.html", mainpage_info=result, message_conut=message_count), 200
 
 # 롤링 페이지 캔들 정보 get
 @rolling.route('/detail-data/<url>/<rolling_id>')
@@ -32,3 +32,36 @@ def get_candle(url, rolling_id):
     message_list = list(db.message.find({'rolling_id': int(rolling_id)}, {'_id': False}))
     print(message_list)
     return jsonify({'message_list': message_list}), 200
+
+
+# 메시지 비밀번호 확인
+# @rolling.route('/message/check', methods=['POST'])
+# def check_message_password():
+#     if(#비밀번호 일치시):
+#         success = True
+#         message = '비밀번호 일치'
+#     else:
+#         success = False
+#         message = '비밀번호 일치하지 않음'
+#     return jsonify({'success': success, 'message': message})
+#
+
+
+# 메시지 내용 수정
+@rolling.route('/message/update', methods=['POST'])
+def message_update():
+    message_id_receive = request.form['message_id']
+    message_content_receive = request.form['message_content']
+
+    db.message.update_one({'message_id': int(message_id_receive)}, {'$set': {'message_content': message_content_receive}})
+    return jsonify({'message': '수정 완료!'}), 200
+
+
+# 메시지 삭제
+@rolling.route('/message/removal', methods=['POST'])
+def message_delete():
+    message_id_receive = request.form['message_id']
+    print(message_id_receive)
+    db.message.delete_one({'message_id': int(message_id_receive)})
+
+    return jsonify({'message': '삭제 완료!'}), 200

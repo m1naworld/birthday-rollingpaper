6ddd695e219d4dbea7e042890304acc33f5facc9
SooBin An <jejupalette@naver.com>
2022-10-27 10:14:20 +0900

    feat : cake 페이지 jwt 추가

--- routes/cake.py
+++ routes/cake.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify
 from db import db
 import bcrypt
+from flask_jwt_extended import (get_jwt_identity, jwt_required)
 
 cake = Blueprint("cake", __name__, template_folder="templates")
 
@@ -8,20 +9,24 @@ cake = Blueprint("cake", __name__, template_folder="templates")
 def question():
     return render_template("cake.html")
 
-@cake.route("/cake", methods=["POST"])
+@cake.route("/makecake", methods=["POST"])
+@jwt_required()
 def sel_cake():
-    cake_receive = request.form['cake_give']
-    nickname_receive = request.form['nickname_give']
-    birth_receive = request.form['birth_give']
+    print('?')
+    user_id = get_jwt_identity()
+    print(user_id)
+    cake_receive = request.form['cake_id']
+    nickname_receive = request.form['user_nickname']
+    birth_receive = request.form['birth']
 
     all_cake = list(db.rollingpaper.find({}, {'_id': False}))
     last_cake_num = all_cake.pop()['rolling_id']
 
     rolling_id = str(int(last_cake_num) + 1)
 
-    print(cake_receive, nickname_receive, birth_receive, rolling_id )
+    print(cake_receive, nickname_receive, birth_receive, rolling_id, user_id)
     # 세션에서 현재 유저 받아오기
-    user_id = request.form['temp_user']
+    # user_id = request.form['temp_user']
 
     url = str(bcrypt.hashpw(rolling_id.encode('utf-8'), bcrypt.gensalt()), 'utf-8')
     # print(bcrypt.hashpw(rolling_id.encode('utf-8'), bcrypt.gensalt()))
@@ -30,8 +35,11 @@ def sel_cake():
            'user_id': user_id, 'cake_id': cake_receive, 'url': url}
     db.rollingpaper.insert_one(doc)
 
-    return jsonify({'msg': '케이크가 만들어졌어요!'})
+    return jsonify({'msg': '케이크가 만들어졌어요!', 'user_id': user_id})
 #
+
+
+
 # rolling_id = '1234';
 # print(bcrypt.hashpw(rolling_id.encode('utf-8'), bcrypt.gensalt()))
 #

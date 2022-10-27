4458b1c1b3b77850a3a60e24a627cd5ff3f14c80
mina <m1nna@naver.com>
2022-10-27 21:13:41 +0900

    style: 안쓰는 코드 삭제

--- routes/rollingpaper.py
+++ routes/rollingpaper.py
@@ -4,29 +4,6 @@ import bcrypt
 
 rolling = Blueprint("rolling", __name__, template_folder="templates")
 
-
-# 롤링 페이퍼 페이지 기본 정보 get
-# @rolling.route('/<url>')
-# def get_rollingpaper(url):
-#     print(url)
-#
-#     # test 주석
-#     result = {'url': 'mina', 'rolling_id': 1, 'user_nickname': "mina", "cake_id": "choco"}
-#     message_count = db.message.count_documents({'rolling_id': 1})
-#
-#     # result = db.rollingpaper.find_one({'url': url})
-#     # message_count = db.message.count_documents({'rolling_id': result['rolling_id']})
-#
-#     print(result)
-#     print(message_count)
-#
-#     # if(# 토큰 없을 경우 ):
-#     return render_template("guestMain.html", mainpage_info=result, message_count=message_count), 200
-#
-#     # else: # 토큰 있을 경우
-#     # return render_template("rollingpaper.html", mainpage_info=result, message_count=message_count), 200
-
-
 # 롤링 페이퍼 페이지 기본 정보 get
 # 로그인 유저용
 @rolling.route('/')
@@ -34,16 +11,11 @@ def get_my_rollingpaper():
 
     url = request.args.get('key')
     print(url)
-    # test 주석
-    # result = {'url': 'mina', 'rolling_id': 1, 'user_nickname': "mina", "cake_id": "choco"}
-    # message_count = db.message.count_documents({'rolling_id': 1})
 
     result = db.rollingpaper.find_one({'url': url}, {'_id': False})
     print(result)
 
     message_count = db.message.count_documents({'rolling_id': result['rolling_id']})
-    print(message_count)
-
     return render_template("rollingpaper.html", mainpage_info=result, message_count=message_count), 200
 
 
